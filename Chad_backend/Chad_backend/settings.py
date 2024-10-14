9c5a87efd3908ad5a29c3df7418ec410f8569834
Hassanibrar632 <hassanibrar632@gmail.com>
2024-10-14 09:38:04 +0500

    User Handling Basic Structure
    Created a basic app in Django to make handle users related APIs like signing up or signing in and all that stuff.

--- Chad_backend/Chad_backend/settings.py
+++ Chad_backend/Chad_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "dataset",
+    "users",
 ]
 
 MIDDLEWARE = [
--- Chad_backend/Chad_backend/urls.py
+++ Chad_backend/Chad_backend/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('dataset/', include("dataset.urls")),
+    path('user/', include("users.urls"))
 ]
