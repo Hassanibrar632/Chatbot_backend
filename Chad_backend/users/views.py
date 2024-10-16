35f0f7599a083145f83f009be6473808b3426050
Hassanibrar632 <hassanibrar632@gmail.com>
2024-10-16 17:07:53 +0500

    Update views.py

--- Chad_backend/users/views.py
+++ Chad_backend/users/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the user index.")
+
+@api_view(['GET'])
+# Create API to add user for the chadbot
+def register_user(request):
+        # perform aunthication
+        return Response({'Register': 'True or False', 'error': 'error'})
+
+# Create API for Loginng in to the chadbot
+
