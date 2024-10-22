673b78df579d266749fed4e324d1365fd14ac404
Hassanibrar632 <hassanibrar632@gmail.com>
2024-10-22 18:05:24 +0500

    dataset model
    created a dataset models for storing the dataset information in the database

--- Chad_backend/dataset/models.py
+++ Chad_backend/dataset/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Dataset(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    data_type = models.BooleanField()
+    base_path = models.CharField(max_length=50)
+    modified_path = models.CharField(max_length=50, null=True)
+    user = models.PositiveIntegerField()
+    Created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(null=True)
+
+    """
+    So we have created a Dataset table to save the rescords of datafiles unloaded by the users
+    We have these columns:
+    (ID auto created)
+    1. name(of the dataset)
+    2. data_type(catagorical or regssive)
+    3. base_path(untouched base file of the dataset)
+    4. modified_path()
+    5. user [ID](created the dataset)
+    6. created date
+    """
--- Chad_backend/dataset/views.py
+++ Chad_backend/dataset/views.py
@@ -1,5 +1,66 @@
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Dataset
+import json
 
+# Create API to add dataset for the user
+@api_view(['POST'])
+def get_data(request):
+    try:
+        # Gather all the data for the user
+        data = json.loads(request.body)
+        # user = data.get('user')
+        
+        # Check the data
+        if not data:
+            return Response({'result': False, 'error': 'Missing required fields'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Check if same dataset exsists
+        if Dataset.objects.filter().exists():
+            return Response({'result': False, 'error': 'User Already exits with the same email'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            # create a dataset into the database
+            
+            return Response({'result': True, 'message': 'User registered successfully!'}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'result': False, 'error': e}, status=status.HTTP_400_BAD_REQUEST)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    except Exception as e:
+        return Response({'result': False, 'error': e}, status=status.HTTP_400_BAD_REQUEST)
+
+# API to get data details to display on page
+@api_view(['GET'])
+def get_datasets(requests, pk):
+    try:
+        # Get data based on the user ID
+        data = Dataset.objects.filter(user=pk).all()
+
+        # Extract the data from the user object
+        
+        
+        return Response({'result': True, 'user': data}, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return Response({'result': False, 'error': e}, status=status.HTTP_400_BAD_REQUEST)
+    
+# API to login into the account
+@api_view(['POST'])
+def load_data(request):
+    try:
+        # Gather all the data form the user's request 
+        data = json.loads(request.body)
+        pk = data
+        # Get data based on username else with email
+        if Dataset.objects.filter(pk=pk).exists():
+            temp = Dataset.objects.filter(pk=pk).first()
+        else:
+            return Response({'result': False, 'error': "User or email doesn't exist in the datbase"}, status=status.HTTP_400_BAD_REQUEST)
+        
+    except Exception as e:
+        return Response({'result': False, 'error': e}, status=status.HTTP_400_BAD_REQUEST)
+    
+########################################################
+#  All the Dataset functions(basic) are done for now.  #
+########################################################
